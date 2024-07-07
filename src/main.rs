471064a5242b56ddcc6515a16c98bb06948d710a
Mikk <mikk.loomets@gmail.com>
2024-07-07 18:07:37 +0300

    Add IMU code

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,12 @@ use std::error::Error;
 use std::thread;
 use std::time::Duration;
 
-use icm20608g as imu;
-use icm20608g::structs::{AccelConfig1, Config, PowerManagement1, ReadRegister, WriteRegister};
 use rppal::i2c::I2c;
 use rppal::system::DeviceInfo;
 
 #[allow(dead_code)]
 mod consts;
+mod imu;
 #[allow(dead_code)]
 mod rgb;
 
@@ -19,24 +18,13 @@ async fn main() -> Result<(), Box<dyn Error>> {
     // let mut rgb = rgb::init_leds()?;
     let mut i2c = I2c::new()?;
     println!("I2c: {:?}", i2c.capabilities());
-    imu::who_am_i(&mut i2c);
-    let mut pwr_mgmt = PowerManagement1::new(&mut i2c)?;
-    pwr_mgmt.print_table(&mut i2c)?;
-    pwr_mgmt.sleep = false;
-    pwr_mgmt.write(&mut i2c)?;
-    let mut config = Config::new(&mut i2c)?;
-    config.print_table(&mut i2c)?;
-    config.dlpf_cfg = 6;
-    config.write(&mut i2c)?;
-    let accel_config = AccelConfig1::new(&mut i2c)?;
-    accel_config.print_table(&mut i2c)?;
+
+    imu::setup(&mut i2c)?;
+
     for _ in 0..100 {
-        let acceleration = imu::accel_data(&mut i2c)?;
-        println!(
-            "Acceleration x: {} y: {} z: {}",
-            acceleration.x, acceleration.y, acceleration.z
-        );
-        thread::sleep(Duration::from_millis(1000));
+        let accel = imu::get_accel(&mut i2c)?;
+        println!("Acceleration x: {} y: {} z: {}", accel.x, accel.y, accel.z);
+        thread::sleep(Duration::from_millis(500));
     }
 
     Ok(())

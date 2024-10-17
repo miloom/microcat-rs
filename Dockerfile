b860293a6970e5924d7589900bd647541aee17ba
Mikk Loomets <miloom@taltech.ee>
2024-10-17 17:48:19 +0300

    Remove invalid files

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM arm64v8/ubuntu:22.04
-
-RUN apt-get update
-RUN apt-get install -y bash
-RUN rm -rf /var/lib/apt/lists/*
-
-CMD ["/bin/bash"]
--- src/pressure.rs
+++
@@ -1,9 +0,0 @@
-use rppal::i2c::I2c;
-use std::error::Error;
-
-pub fn test_pressure(i2c:&mut I2c) -> Result<(), Box<dyn Error>> {
-    i2c.set_slave_address(0x76)?;
-    let in_buffer: [u8; 10]; 
-
-    Ok(())
-}

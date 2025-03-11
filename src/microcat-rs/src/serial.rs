5abc2a1820906fc3a0ada545e443dac7878783af
Mikk Loomets <miloom@taltech.ee>
2025-03-11 09:13:45 +0200

    Remove panic from serial

--- src/microcat-rs/src/serial.rs
+++ src/microcat-rs/src/serial.rs
@@ -7,22 +7,22 @@ use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::sync::mpsc::Sender;
 
 #[cfg(feature = "proto")]
+mod imu;
+#[cfg(feature = "proto")]
 #[allow(clippy::all, clippy::nursery, clippy::pedantic)]
 mod message;
-#[cfg(feature = "proto")]
-mod tone_detector;
-#[cfg(feature = "proto")]
-mod imu;
 #[cfg(feature = "proto")]
 mod motor;
 #[cfg(feature = "proto")]
 mod pressure;
+#[cfg(feature = "proto")]
+mod tone_detector;
 pub async fn read(
     // serial: &mut rppal::uart::Uart,
     serial: &mut Box<dyn SerialPort>,
     message_buffer: &mut BytesMut,
     initialized: &mut bool,
-    tx: &mut Sender<crate::Telemetry>
+    tx: &mut Sender<crate::Telemetry>,
 ) {
     // let mut buf = bytes::BytesMut::with_capacity(1024);
     let mut buf: [u8; 100] = [0; 100];
@@ -43,85 +43,99 @@ pub async fn read(
                 let message = message_buffer.split_to(end + 1);
                 //println!("Message {message:?}");
                 let mut dest = [0u8; 256];
-                let len = cobs::decode(message.iter().as_slice(), &mut dest)
-                    .expect("Expected to be able to decode COBS packet");
+                let len = cobs::decode(message.iter().as_slice(), &mut dest);
+                if len.is_err() {
+                    return;
+                }
+                let len = len.unwrap();
 
                 #[allow(unused_variables)]
                 let message = &dest[..len];
 
                 #[cfg(feature = "proto")]
-                let decoded =
-                    message::Message::decode(message).inspect_err(|e| println!("{e}"));
+                let decoded = message::Message::decode(message).inspect_err(|e| println!("{e}"));
                 #[cfg(not(feature = "proto"))]
                 let decoded: Result<(), anyhow::Error> = Ok(());
 
                 #[allow(unused_variables)]
                 if let Ok(msg) = decoded {
-
                     #[cfg(feature = "proto")]
-                    match msg.data.unwrap() {
-                        message::message::Data::Imu(msg) => {
+                    match msg.data {
+                        Some(message::message::Data::Imu(msg)) => {
                             if let Some(gyro) = msg.gyro {
                                 if let Some(accel) = msg.accel {
-                                    tx.send(Telemetry::Imu(microcat_msgs::msg::Imu {
-                                        gyro_x: gyro.x as f32 / 1000.0,
-                                        gyro_y: gyro.y as f32 / 1000.0,
-                                        gyro_z: gyro.z as f32 / 1000.0,
-                                        accel_x: accel.x,
-                                        accel_y: accel.y,
-                                        accel_z: accel.z,
-                                    })).await.unwrap();
+                                    let _ = tx
+                                        .send(Telemetry::Imu(microcat_msgs::msg::Imu {
+                                            gyro_x: gyro.x as f32 / 1000.0,
+                                            gyro_y: gyro.y as f32 / 1000.0,
+                                            gyro_z: gyro.z as f32 / 1000.0,
+                                            accel_x: accel.x,
+                                            accel_y: accel.y,
+                                            accel_z: accel.z,
+                                        }))
+                                        .await;
                                 }
                             }
                         }
-                        message::message::Data::MotorTarget(msg) => {
-
+                        Some(message::message::Data::MotorTarget(msg)) => {}
+                        Some(message::message::Data::MotorPosition(msg)) => {
+                            let _ = tx
+                                .send(Telemetry::MotorPosition(microcat_msgs::msg::MotorStatus {
+                                    location: if let Ok(loc) =
+                                        motor::Location::try_from(msg.location)
+                                    {
+                                        match loc {
+                                            motor::Location::FrontLeft => {
+                                                microcat_msgs::msg::MotorStatus::FRONT_LEFT
+                                            }
+                                            motor::Location::FrontRight => {
+                                                microcat_msgs::msg::MotorStatus::FRONT_RIGHT
+                                            }
+                                            motor::Location::BackLeft => {
+                                                microcat_msgs::msg::MotorStatus::REAR_LEFT
+                                            }
+                                            motor::Location::BackRight => {
+                                                microcat_msgs::msg::MotorStatus::REAR_RIGHT
+                                            }
+                                        }
+                                    } else {
+                                        255
+                                    },
+                                    position: msg.position,
+                                }))
+                                .await;
                         }
-                        message::message::Data::MotorPosition(msg) => {
-                            tx.send(Telemetry::MotorPosition(microcat_msgs::msg::MotorStatus {
-                                location: if let Ok(loc) = motor::Location::try_from(msg.location) {
-                                    match loc {
-                                        motor::Location::FrontLeft => {
-                                            microcat_msgs::msg::MotorStatus::FRONT_LEFT
+                        Some(message::message::Data::ToneDetectorStatus(msg)) => {
+                            let _ = tx
+                                .send(Telemetry::ToneDetector(microcat_msgs::msg::ToneDetector {
+                                    location: if let Ok(loc) =
+                                        tone_detector::Location::try_from(msg.location)
+                                    {
+                                        match loc {
+                                            tone_detector::Location::Left => {
+                                                microcat_msgs::msg::ToneDetector::LEFT
+                                            }
+                                            tone_detector::Location::Right => {
+                                                microcat_msgs::msg::ToneDetector::RIGHT
+                                            }
                                         }
-                                        motor::Location::FrontRight => {
-                                            microcat_msgs::msg::MotorStatus::FRONT_RIGHT
-                                        }
-                                        motor::Location::BackLeft => {
-                                            microcat_msgs::msg::MotorStatus::REAR_LEFT
-                                        }
-                                        motor::Location::BackRight => {
-                                            microcat_msgs::msg::MotorStatus::REAR_RIGHT
-                                        }
-                                    }
-                                } else {
-                                    255
-                                },
-                                position: msg.position,
-                            })).await.unwrap();
+                                    } else {
+                                        255
+                                    },
+                                    is_active: msg.is_high,
+                                }))
+                                .await;
                         }
-                        message::message::Data::ToneDetectorStatus(msg) => {
-                            tx.send(Telemetry::ToneDetector(microcat_msgs::msg::ToneDetector {
-                                location: if let Ok(loc) = tone_detector::Location::try_from(msg.location) {
-                                    match loc {
-                                        tone_detector::Location::Left => {
-                                            microcat_msgs::msg::ToneDetector::LEFT
-                                        }
-                                        tone_detector::Location::Right => {
-                                            microcat_msgs::msg::ToneDetector::RIGHT
-                                        }
-                                    }
-                                } else {
-                                    255
-                                },
-                                is_active: msg.is_high
-                            })).await.unwrap();
+                        Some(message::message::Data::PressureData(msg)) => {
+                            let _ = tx
+                                .send(Telemetry::PressureData(microcat_msgs::msg::PressureData {
+                                    pressure: msg.pressure as f32 / 100.0,
+                                    temperature: msg.temperature as f32 / 100.0,
+                                }))
+                                .await;
                         }
-                        message::message::Data::PressureData(msg) => {
-                            tx.send(Telemetry::PressureData(microcat_msgs::msg::PressureData {
-                                pressure: msg.pressure as f32 / 100.0,
-                                temperature: msg.temperature as f32 / 100.0,
-                            })).await.unwrap();
+                        None => {
+                            return;
                         }
                     }
                 } else {
@@ -161,18 +175,10 @@ pub fn write(serial: &mut Box<dyn SerialPort>, command: Command) {
                 amplitude: pos.amplitude,
                 frequency: (pos.frequency * 1000.0) as u32,
                 location: match pos.location {
-                    MotorLocation::FrontLeft => {
-                        motor::Location::FrontLeft.into()
-                    }
-                    MotorLocation::FrontRight => {
-                        motor::Location::FrontRight.into()
-                    }
-                    MotorLocation::RearRight => {
-                        motor::Location::BackRight.into()
-                    }
-                    MotorLocation::RearLeft => {
-                        motor::Location::BackLeft.into()
-                    }
+                    MotorLocation::FrontLeft => motor::Location::FrontLeft.into(),
+                    MotorLocation::FrontRight => motor::Location::FrontRight.into(),
+                    MotorLocation::RearRight => motor::Location::BackRight.into(),
+                    MotorLocation::RearLeft => motor::Location::BackLeft.into(),
                 },
             };
             message::Message {
@@ -182,7 +188,10 @@ pub fn write(serial: &mut Box<dyn SerialPort>, command: Command) {
     };
 
     let mut buf = BytesMut::new();
-    message.encode(&mut buf).unwrap();
+    if message.encode(&mut buf).is_err() {
+        eprintln!("Failed to encode");
+        return;
+    }
     let mut dest = [0u8; 128];
     let len = cobs::encode(buf.iter().as_slice(), &mut dest);
     dest[len] = 0;

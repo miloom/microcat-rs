6f2f1207389b468d1da30be34e14a87d6528b051
Mikk Loomets <miloom@taltech.ee>
2025-04-15 03:24:28 +0300

    Test camera code

--- src/microcat-rs/src/camera.rs
+++ src/microcat-rs/src/camera.rs
@@ -1,1 +1,11 @@
-struct Camera {}
+use libcamera;
+pub struct Camera {
+    camera: libcamera::camera::Camera,
+}
+
+impl Camera {
+    pub fn new() -> Camera {
+        let manager = libcamera::camera_manager::CameraManager::new().unwrap();
+        Camera {}
+    }
+}

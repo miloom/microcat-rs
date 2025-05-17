5099b13c0d51bee27c85302af82b073aee738890
Mikk Loomets <mikk.loomets@gmail.com>
2025-05-17 18:50:45 +0300

    Increase delay on serial transfer

--- src/microcat-rs/src/serial.rs
+++ src/microcat-rs/src/serial.rs
@@ -208,7 +208,7 @@ pub async fn write(serial: &mut SerialStream, command: Command) {
         match serial.write_all(&[*byte]).await {
             Ok(_) => {
                 serial.flush().await.unwrap(); // Ensure data hits the wire
-                tokio::time::sleep(tokio::time::Duration::from_micros(2500)).await;
+                tokio::time::sleep(tokio::time::Duration::from_micros(3000)).await;
             }
             Err(e) => {
                 error!("Failed to write to serial: {}", e);
